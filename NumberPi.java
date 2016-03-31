5be41b93a4dcdfe17b5034596d3262460bebd21e
Eugene Farenyuk <eugene.farenyuk@gmail.com>
2016-03-31 13:43:33 -0400

    Do 10 outputs with increasing number of iterations in each.
    In 10 outputs num. of iterations are from 1M to 10M.
    10 outputs are:
    3.1415916535897743
    3.141592153589724
    3.1415923202563736
    3.141592403589655
    3.1415924535897797
    3.1415924869231824
    3.1415925107326004
    3.1415925285896966
    3.141592542478652
    3.1415925535897915
    => starting from 2nd iteration the 7th s.f. is 2 and it is stable.
    => we need approx. 1-2 million iterations to reach the given task of 6
    s.f.

--- NumberPi.java
+++ NumberPi.java
@@ -1,11 +1,18 @@
 // Task: 1.Compute Number Pi using formula: Pi = 4* (1 -1/3 +1/5 -1/7 +...).
+//       2.(not done yet) Limit the result to six significant digits.
 import java.lang.*;
 public class NumberPi {
     public static void main(String args[]) {        
-        double Pi=0;
         // in sequence {1,3,5,7,...} the n-th term = 2*n-1.
         // to alternate plus and minus use Math.pow(-1.0, i+1.0).
-        for(int i=1; i<=1000000; i++)  Pi = Pi + 1.0/(2.0*i-1) * Math.pow(-1.0, i+1.0);
-        Pi = 4*Pi; 
-        System.out.println(Pi);
+        int iMax= 1000000; // this is the max number of iterations.
+        for (int j=0; j<10; j++) {
+            double Pi=0;
+            // in sequence {1,3,5,7,...} the n-th term = 2*n-1.
+            // to alternate plus and minus use Math.pow(-1.0, i+1.0).
+            for(int i=1; i<=iMax; i++)  Pi = Pi + 1.0/(2.0*i-1) * Math.pow(-1.0, i+1.0);
+            Pi = 4*Pi; 
+            System.out.println(Pi);
+            iMax=iMax+1000000;
+        }
 }}
