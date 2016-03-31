8bf25fdc9302031a8491ae63e9089e5ce4329c63
Eugene Farenyuk <eugene.farenyuk@gmail.com>
2016-03-31 13:52:57 -0400

    One million iterations is hard coded.
    output: Pi = 3.14159 (limited to six s.f.)
    1 million of iterations is necessary and sufficient for task #2.
    The number 1 million was determined by testing (compare outputs to the
    commonly known exact Pi).

--- NumberPi.java
+++ NumberPi.java
@@ -5,14 +5,12 @@ public class NumberPi {
     public static void main(String args[]) {        
         // in sequence {1,3,5,7,...} the n-th term = 2*n-1.
         // to alternate plus and minus use Math.pow(-1.0, i+1.0).
-        int iMax= 1000000; // this is the max number of iterations.
-        for (int j=0; j<10; j++) {
-            double Pi=0;
-            // in sequence {1,3,5,7,...} the n-th term = 2*n-1.
-            // to alternate plus and minus use Math.pow(-1.0, i+1.0).
-            for(int i=1; i<=iMax; i++)  Pi = Pi + 1.0/(2.0*i-1) * Math.pow(-1.0, i+1.0);
-            Pi = 4*Pi; 
-            System.out.println(Pi);
-            iMax=iMax+1000000;
-        }
+        int iMax= 1000000; // max number of iterations necessary and sufficient for task #2.
+        // the number 1 million was determined by testing (compare outputs to the commonly known exact Pi).
+        double Pi=0;
+        // in sequence {1,3,5,7,...} the n-th term = 2*n-1.
+        // to alternate plus and minus use Math.pow(-1.0, i+1.0).
+        for(int i=1; i<=iMax; i++)  Pi = Pi + 1.0/(2.0*i-1) * Math.pow(-1.0, i+1.0);
+        Pi = 4*Pi; 
+        System.out.printf("Pi = %.5f (limited to six s.f.)\n", Pi);
 }}
